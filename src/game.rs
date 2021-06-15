407f8beae1697a855782a9ac85cb7eb9dadb541a
Scotow <contact@scotow.com>
2021-06-15 19:34:22 +0200

    Refactor game and player arc/mutex, fix collision cell removal

--- src/game.rs
+++ src/game.rs
@@ -4,13 +4,16 @@ use std::time::Duration;
 use futures::future::join_all;
 use std::fmt::{Debug, Write};
 use warp::ws::{Message, WebSocket};
-use futures::{SinkExt};
+use futures::{SinkExt, StreamExt};
 use tokio::sync::Mutex;
 use std::sync::Arc;
 use tokio::task;
 use futures::stream::SplitStream;
+use crate::coord::Coord;
+use rand::Rng;
+use std::collections::HashMap;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Cell {
     Empty,
     Perk,
@@ -29,85 +32,123 @@ impl Into<char> for Cell {
 }
 
 pub struct Game {
-    grid: Arc<Mutex<Vec<Vec<Cell>>>>,
-    players: Arc<Mutex<Vec<Arc<Player>>>>,
+    inner: Arc<Mutex<Inner>>,
+}
+
+struct Inner {
+    grid: Vec<Vec<Cell>>,
+    players: HashMap<usize, Arc<Mutex<Player>>>,
 }
 
 impl Game {
     pub fn new() -> Self {
         Self {
-            grid: Arc::new(Mutex::new(vec![vec![Cell::Empty; 16]; 16])),
-            players: Arc::new(Mutex::new(Vec::new())),
+            inner: Arc::new(Mutex::new(Inner {
+                grid: vec![vec![Cell::Empty; 16]; 16],
+                players: HashMap::new(),
+            }))
         }
     }
 
     pub async fn run(&self) {
         loop {
-            let players = self.players.lock().await;
-            self.walk_snakes(&*players).await;
-            self.broadcast_grid(&*players).await;
-            drop(players);
+            let mut inner = self.inner.lock().await;
+            self.walk_snakes(&mut inner).await;
+            self.broadcast_grid(&inner).await;
+            drop(inner);
 
             sleep(Duration::from_millis(50)).await;
         }
     }
 
-    async fn walk_snakes(&self, players: &[Arc<Player>]) {
-        let changes = join_all(players.iter().map(|p| p.walk())).await;
-        let mut grid = self.grid.lock().await;
-        changes.into_iter()
-            .filter_map(|c| c)
-            .for_each(|(r, n)| {
-                if let Some(removed) = r {
-                    grid[removed.y][removed.x] = Cell::Empty;
-                }
-                grid[n.y][n.x] = Cell::Occupied;
-            });
+    pub async fn add_player(&self, socket: WebSocket) {
+        let mut inner = self.inner.lock().await;
+        let head = self.safe_place(&inner.grid);
+
+        let id = rand::random();
+        let (player, rx) = Player::new(socket, head);
+        let player = Arc::new(Mutex::new(player));
+        self.player_loop(id, Arc::clone(&player), rx);
+
+        inner.grid[head.y][head.x] = Cell::Occupied;
+        inner.players.insert(id, player);
     }
 
-    async fn broadcast_grid(&self, players: &[Arc<Player>]) {
-        let message = Message::text(self.ascii_grid().await);
-        join_all(players.iter()
+    fn player_loop(&self, id: usize, player: Arc<Mutex<Player>>, mut rx: SplitStream<WebSocket>) {
+        let inner = Arc::clone(&self.inner);
+        task::spawn(async move {
+            while let Some(Ok(message)) = rx.next().await {
+                if message.is_close() {
+                    break;
+                }
+                player.lock().await.process(message).await;
+            }
+            // Player left the game from here.
+
+            let mut inner = inner.lock().await;
+            inner.players.remove(&id);
+
+            player.lock().await.body.iter()
+                .for_each(|&c| inner.grid[c.y][c.x] = Cell::Empty);
+        });
+    }
+
+    fn safe_place(&self, grid: &[Vec<Cell>]) -> Coord {
+        let mut rng = rand::thread_rng();
+        loop {
+            let tmp = Coord {
+                x: rng.gen_range(0..16),
+                y: rng.gen_range(0..16),
+            };
+            if grid[tmp.y][tmp.x] == Cell::Empty {
+                return tmp;
+            }
+        }
+    }
+
+    async fn walk_snakes(&self, inner: &mut Inner) {
+        let changes = join_all(
+            inner.players.values().map(|p| {
+                async move {
+                    p.lock().await
+                        .walk().await
+                        .map(|cs| (Arc::clone(p), cs))
+                }
+            })).await;
+
+        for (player, (removed, new)) in changes.into_iter()
+            .filter_map(|e| e) {
+            if let Some(removed) = removed {
+                inner.grid[removed.y][removed.x] = Cell::Empty;
+            }
+            if inner.grid[new.y][new.x] == Cell::Occupied {
+                let mut player = player.lock().await;
+                player.body.iter().skip(1).for_each(|&c| inner.grid[c.y][c.x] = Cell::Empty);
+
+                let head = self.safe_place(&inner.grid);
+                player.respawn(head).await;
+                inner.grid[head.y][head.x] = Cell::Occupied;
+            } else {
+                inner.grid[new.y][new.x] = Cell::Occupied;
+            }
+        }
+    }
+
+    async fn broadcast_grid(&self, inner: &Inner) {
+        let message = Message::text(self.ascii_grid(&inner.grid).await);
+        join_all(inner.players.values()
             .map(|p| {
                 let message = message.clone();
                 async move {
-                    let _ = p.sink.lock().await.send(message).await;
+                    let _ = p.lock().await.sink.send(message).await;
                 }
             })
         ).await;
     }
 
-    pub async fn add_player(&self, socket: WebSocket) {
-        let (player, rx, head) = Player::new(socket);
-        let player = Arc::new(player);
-        self.player_loop(Arc::clone(&player), rx);
-
-        self.grid.lock().await[head.y][head.x] = Cell::Occupied;
-        self.players.lock().await.push(player);
-    }
-
-    fn player_loop(&self, player: Arc<Player>, rx: SplitStream<WebSocket>) {
-        let players = Arc::clone(&self.players);
-        let grid = Arc::clone(&self.grid);
-        task::spawn(async move {
-            player.listen(rx).await;
-
-            let mut players = players.lock().await;
-            if let Some(index) = players.iter().position(|p| p.id == player.id) {
-                players.remove(index);
-            }
-            drop(players);
-
-            let mut grid = grid.lock().await;
-            player.body.lock().await.iter()
-                .for_each(|&c| grid[c.y][c.x] = Cell::Empty);
-        });
-    }
-
-    async fn ascii_grid(&self) -> String {
-        let grid = self.grid.lock().await;
+    async fn ascii_grid(&self, grid: &[Vec<Cell>]) -> String {
         let mut ascii = String::with_capacity(grid.len() * (grid[0].len() + 1));
-        for row in &*grid {
+        for row in grid {
             for &cell in row {
                 ascii.write_char(cell.into()).unwrap();
             }
--- src/player.rs
+++ src/player.rs
@@ -4,93 +4,92 @@ use std::collections::VecDeque;
 use futures::stream::{SplitSink, SplitStream};
 use warp::ws::{Message, WebSocket};
 use futures::{StreamExt};
-use rand::Rng;
 
 #[derive(Debug)]
 pub struct Player {
-    pub id: usize,
-    pub body: Mutex<VecDeque<Coord>>,
-    direction: Mutex<Option<Dir>>,
-    directions_queue: Mutex<VecDeque<Dir>>,
-    growth: Mutex<u16>,
-    pub sink: Mutex<SplitSink<WebSocket, Message>>,
+    pub body: VecDeque<Coord>,
+    direction: Mutex<Direction>,
+    growth: u16,
+    pub sink: SplitSink<WebSocket, Message>,
+}
+
+#[derive(Debug, Default)]
+struct Direction {
+    current: Option<Dir>,
+    queue: VecDeque<Dir>,
 }
 
 impl Player {
-    pub fn new(socket: WebSocket) -> (Self, SplitStream<WebSocket>, Coord) {
+    pub fn new(socket: WebSocket, head: Coord) -> (Self, SplitStream<WebSocket>) {
         let (tx, rx) = socket.split();
-        let mut rng = rand::thread_rng();
-        let head = Coord { x: rng.gen_range(0..16), y: rng.gen_range(0..16) };
         (Self {
-            id: rng.gen(),
-            body: Mutex::new(VecDeque::from(vec![head])),
-            direction: Mutex::new(None),
-            directions_queue: Mutex::new(VecDeque::with_capacity(8)),
-            growth: Mutex::new(5),
-            sink: Mutex::new(tx),
-        }, rx, head)
+            body: VecDeque::from(vec![head]),
+            direction: Mutex::new(Direction::default()),
+            growth: 10,
+            sink:tx,
+        }, rx)
     }
 
-    pub async fn listen(&self, mut rx: SplitStream<WebSocket>) {
-        while let Some(Ok(message)) = rx.next().await {
-            if message.is_close() {
-                break
-            }
+    pub async fn process(&self, message: Message) {
+        let new = match message.as_bytes()[1] {
+            0 => Dir { x: 0, y: -1 },
+            1 => Dir { x: 0, y: 1 },
+            2 => Dir { x: -1, y: 0 },
+            3 => Dir { x: 1, y: 0 },
+            _ => return
+        };
 
-            let new = match message.as_bytes()[1] {
-                0 => Dir { x: 0, y: -1 },
-                1 => Dir { x: 0, y: 1 },
-                2 => Dir { x: -1, y: 0 },
-                3 => Dir { x: 1, y: 0 },
-                _ => return
-            };
-
-            let mut queue = self.directions_queue.lock().await;
-            let last = if let Some(dir) = queue.back().copied() {
-                Some(dir)
-            } else {
-                *self.direction.lock().await
-            };
-            if let Some(dir) = last {
-                if !dir.conflict(&new) {
-                    queue.push_back(new);
-                }
-            } else {
-                queue.push_back(new);
+        let mut direction = self.direction.lock().await;
+        let last = if let Some(dir) = direction.queue.back().copied() {
+            Some(dir)
+        } else {
+            direction.current
+        };
+        if let Some(dir) = last {
+            if !dir.conflict(&new) {
+                direction.queue.push_back(new);
             }
+        } else {
+            direction.queue.push_back(new);
         }
     }
 
-    pub async fn walk(&self) -> Option<(Option<Coord>, Coord)> {
-        let mut current_direction = self.direction.lock().await;
-        let mut queue = self.directions_queue.lock().await;
-        let new_direction = if !queue.is_empty() {
-            let dir = queue.pop_front().unwrap();
-            *current_direction = Some(dir);
+    pub async fn walk(&mut self) -> Option<(Option<Coord>, Coord)> {
+        let mut direction = self.direction.lock().await;
+        let new_direction = if !direction.queue.is_empty() {
+            let dir = direction.queue.pop_front().unwrap();
+            direction.current = Some(dir);
             dir
         } else {
-            match *current_direction {
+            match direction.current {
                 Some(dir) => dir,
                 None => return None,
             }
         };
 
-        let mut body = self.body.lock().await;
-        let current_head = body.get(0).unwrap();
+        let current_head = self.body.get(0).unwrap();
         let new_head = Coord {
             x: (current_head.x as isize + new_direction.x as isize).rem_euclid(16) as usize,
             y: (current_head.y as isize + new_direction.y as isize).rem_euclid(16) as usize,
         };
-        body.push_front(new_head);
+        self.body.push_front(new_head);
 
-        let mut growth = self.growth.lock().await;
-        let tail = if *growth >= 1 {
-            *growth = *growth - 1;
+        let tail = if self.growth >= 1 {
+            self.growth -= 1;
             None
         } else {
-            Some(body.pop_back().unwrap())
+            Some(self.body.pop_back().unwrap())
         };
 
         Some((tail, new_head))
+    }
+
+    pub async fn respawn(&mut self, head: Coord) {
+        self.body.clear();
+        self.body.push_front(head);
+        let mut direction = self.direction.lock().await;
+        direction.current = None;
+        direction.queue.clear();
+        self.growth = 10;
     }
 }
