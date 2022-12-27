deb228e03c0eca4a4276b46ddab6516171a4f42b
scotow <contact@scotow.com>
2022-12-27 22:44:28 +0100

    Add project version in header

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{error::Error as StdError, net::SocketAddr, sync::Arc, u16};
 
 use axum::{
     extract::{Path, State, WebSocketUpgrade},
+    http::{header, HeaderValue},
+    middleware::map_response,
     response::Response,
     routing::get,
     Router, Server,
@@ -23,7 +25,14 @@ async fn main() -> Result<(), Box<dyn StdError + Send + Sync>> {
         .route("/games/:id", get(join_game_handler))
         .with_state(Arc::new(Lobby::new()))
         .route("/", get(asset::handler))
-        .route("/:asset", get(asset::handler));
+        .route("/:asset", get(asset::handler))
+        .layer(map_response(|mut resp: Response| async {
+            resp.headers_mut().insert(
+                header::SERVER,
+                HeaderValue::from_static(concat!("Cobrust v", env!("CARGO_PKG_VERSION"))),
+            );
+            resp
+        }));
 
     Server::bind(&SocketAddr::new("0.0.0.0".parse()?, 8080))
         .http1_title_case_headers(true)
